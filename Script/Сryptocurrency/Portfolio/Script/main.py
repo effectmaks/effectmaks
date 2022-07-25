5e43ef9d4e39f4be2b304b4837b246c5cacadbc4
Maksim Balonin <effectmaks@gmail.com>
2022-07-25 21:52:39 +0300

    Добавлено логирование скрипта

---
+++ Script/Сryptocurrency/Portfolio/Script/main.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+if __name__ == '__main__':
+    logging.info('Наладка логирования')
+
+
+
