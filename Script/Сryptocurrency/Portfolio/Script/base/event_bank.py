558460c25bd24168469e08ef9ed0d8f750d4aeb4
Maksim Balonin <effectmaks@gmail.com>
2022-07-26 23:09:30 +0300

    Описал поля таблицы event_bank.

---
+++ Script/Сryptocurrency/Portfolio/Script/base/event_bank.py
@@ -0,0 +1,20 @@
+import logging
+from peewee import DateTimeField, IntegerField, DoubleField, Model
+from .sqlite.connectSqlite import ConnectSqlite
+from datetime import datetime
+
+
+class Event_bank(Model):
+    """
+    База данных таблица Событий ввода и вывода средств
+    """
+    id = IntegerField()
+    date_time = DateTimeField(default=datetime.now())
+    id_cash = IntegerField()
+    amount = DoubleField()
+    price_avr_fiat = DoubleField()
+    fee = DoubleField()
+
+    class Meta:
+        table_name = 'event_bank'
+        database = ConnectSqlite.get_connect()
