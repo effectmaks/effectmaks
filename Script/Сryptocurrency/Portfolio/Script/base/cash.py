6188da8e8e959ac064357cb5137db777b1805d65
Maksim Balonin <effectmaks@gmail.com>
2022-07-27 21:27:07 +0300

    Описал модель cash, добавил метод add_cash (Добавление счета монеты)

--- Script/Сryptocurrency/Portfolio/Script/base/cash.py
+++ Script/Сryptocurrency/Portfolio/Script/base/cash.py
@@ -9,11 +9,11 @@ class Cash(Model):
     База данных таблица Счета в сейфе
     """
     id = IntegerField()
-    date_time_update = DateTimeField(default=datetime.now())
+    date_time = DateTimeField(default=datetime.now())
     id_safe = IntegerField()
     coin = TextField()
-    amount = DoubleField()
-    price_avr_fiat = DoubleField()
+    amount_buy = DoubleField(default=0)
+    price_buy_fiat = DoubleField(default=0)
 
     class Meta:
         table_name = 'cash'
@@ -21,15 +21,25 @@ class Cash(Model):
 
 
 class ModelCash:
+    __name_model = 'cash'
 
     @classmethod
-    def add_cash(cls, id_safe: int, coin: str, amount: float, price_avr_fiat: float):
+    def add_cash(cls, id_safe: int, coin: str, amount_buy: float, price_buy_fiat: float):
+        """
+        Добавление счета монеты
+        :param id_safe: ID сейфа
+        :param coin: Монета
+        :param amount_buy: Количество купить
+        :param price_buy_fiat: Цена покупки
+        """
         try:
             id_cash = Cash.create(id_safe=id_safe,
-                                    coin=coin,
-                                    amount=amount,
-                                    price_avr_fiat=price_avr_fiat)
+                        coin=coin,
+                        amount_buy=amount_buy,
+                        price_buy_fiat=price_buy_fiat)
+            logging.info(f'Добавлен счет ID:{id_cash} id_safe:{id_safe} coin:{coin} amount_buy:{amount_buy} '
+                         f'price_buy_fiat:{price_buy_fiat}')
         except Exception as err:
-            raise ExceptionInsert('cash', str(err))
+            raise ExceptionInsert(cls.__name_model, str(err))
 
 
