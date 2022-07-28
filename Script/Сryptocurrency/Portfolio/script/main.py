824901870c0c5205a8b2c7777aa27baf11dc3d08
Maksim Balonin <effectmaks@gmail.com>
2022-07-28 22:04:41 +0300

    Добавлена библиотека dotenv - скрывает API ключи

--- Script/Сryptocurrency/Portfolio/script/main.py
+++ Script/Сryptocurrency/Portfolio/script/main.py
@@ -4,6 +4,11 @@ from base.coin import ModelCoin
 from base.cash import ModelCash
 from base.safeuser import ModelSafeuser
 
+import os
+from telebot import TeleBot
+from dotenv import load_dotenv
+from telegram_bot.api.users import Users
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
@@ -13,14 +18,30 @@ logging.basicConfig(
     ]
 )
 
+
 if __name__ == '__main__':
-    logging.info('Наладка логирования')
     try:
         #ModelCoin.test_coin('SOL')
-        #ModelCash.add_cash(1, 'ETH', 1.0, 10300.0)
         ModelSafeuser.test_safe(1, 2)
+        #ModelCash.add_cash(1, 'ETH', 1.0, 10300.0)
+
     except Exception as e:
         print(str(e))
+
+    load_dotenv('telegram_bot/api/keys.env')
+    logging.info('Подключение TeleBot')
+    bot_telegram = TeleBot(os.getenv('BOT_TOKEN'))
+    logging.info('Подключение TeleBot - успешно')
+
+    @bot_telegram.message_handler(content_types=['text'])
+    def new_message(message):
+        Users.message_info(bot_telegram, message)
+
+    @bot_telegram.callback_query_handler(func=lambda call: True)
+    def click_button(call):
+        Users.button_info(bot_telegram, call)
+
+    bot_telegram.polling(none_stop=True, interval=0)
 
 
 
