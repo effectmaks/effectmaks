4ec07eb3850f830b53dec1f0e77b31f6f0d6882a
Maksim Balonin <effectmaks@gmail.com>
2022-08-03 22:47:10 +0300

    Класс ModesWork, описал работу режима работы /input и /help

--- Script/Сryptocurrency/Portfolio/script/telegram_bot/controlBot.py
+++ Script/Сryptocurrency/Portfolio/script/telegram_bot/controlBot.py
@@ -1,10 +1,7 @@
 import logging
 from .api.telegramApi import ConnectTelebot
-
-
-class ModesWork:
-    COMMAND_START = '/start'
-    COMMAND_HELP = '/help'
+from business_model.operationBank import OperationBank
+from .api.modesWork import ModesWork
 
 
 class ControlBot:
@@ -15,26 +12,48 @@ class ControlBot:
 
     def __init__(self, connect_telebot: ConnectTelebot):
         self.__connect_telebot: ConnectTelebot = connect_telebot
+        self._command_now = ModesWork.NONE
+        self._operaton_bank = None
 
-    def new_message(self, message):
+    def new_message(self, message_str: str):
         """
         Пришло сообщение от юзера
         :param message: Объект с текстом юзера
         """
-        logging.info(f'Принято сообщение: {message.text}')
-        if self._simple_mode(message):
+        logging.info(f'Принято сообщение: {message_str}')
+        if self._simple_mode(message_str):
+            return
+        elif self._input_mode(message_str):
             return
 
-    def _simple_mode(self, message) -> bool:
+    def _input_mode(self, message_str: str) -> bool:
+        """
+        Режим пополнения средств на сейф из вне.
+        """
+        if message_str == ModesWork.COMMAND_INPUT:
+            self._command_now = ModesWork.COMMAND_INPUT
+            self._operaton_bank = OperationBank(self.__connect_telebot, self._command_now)
+
+        if self._command_now == ModesWork.COMMAND_INPUT:
+            try:
+                self._operaton_bank.work(message_str)
+            except Exception as err:
+                logging.error(f'_input_mode: {str(err)}')
+                self.__connect_telebot.send_text(f'Команда {self._command_now} завершена не успешно.')
+                self._command_now = ModesWork.NONE
+            return True
+        return False
+
+    def _simple_mode(self, message_str: str) -> bool:
         """
         Выпоняет команды /start и /help
         :param message: Объект текстом юзера
         :return: True выполнена команда
         """
-        if message.text == ModesWork.COMMAND_START:
+        if message_str == ModesWork.COMMAND_START:
             self._send_text_bot_start()
             return True
-        elif message.text == ModesWork.COMMAND_HELP:
+        elif message_str == ModesWork.COMMAND_HELP:
             self._send_text_bot_help()
             return True
         return False
@@ -54,5 +73,6 @@ class ControlBot:
         :param id_user: ID пользователя
         """
         logging.info(f'Режим: Помощь')
-        text_send = f'{self.__NAME_BOT} выполняет команды:'
+        text_send = f'{self.__NAME_BOT} выполняет команды: \n' \
+                    f'{ModesWork.COMMAND_INPUT} - пополнить счет'
         self.__connect_telebot.send_text(text_send)
