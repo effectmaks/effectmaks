03fa30b888d6e8641e08122d8457af42bf3fce28
Maksim Balonin <effectmaks@gmail.com>
2022-08-04 22:44:16 +0300

    Добавил шаги опроса какой показать тип сейфа, и показать все сейфы этого типа юзера

--- Script/Сryptocurrency/Portfolio/script/business_model/operationBank.py
+++ Script/Сryptocurrency/Portfolio/script/business_model/operationBank.py
@@ -1,7 +1,14 @@
 import logging
 from telegram_bot.api.telegramApi import ConnectTelebot
 from base.safeuser import ModelSafeuser
+from base.safelist import Safetypes
 from telegram_bot.api.modesWork import ModesWork
+
+
+class ExceptionOperationBank(Exception):
+    def __init__(self, err_message: str = ''):
+        logging.error(err_message)
+        super().__init__(err_message)
 
 
 class OperationBank:
@@ -19,7 +26,7 @@ class OperationBank:
         if self._work_next_function(message_str):  # функция выполнилась
             return
         if self._command_now == ModesWork.COMMAND_INPUT:
-            self._step_exhange(message_str)
+            self._input_exhange_type(message_str)
 
     def _set_next_function(self, fnc):
         """
@@ -42,13 +49,24 @@ class OperationBank:
                 self._add_next_function = False
             return True  # Выполнилась функция
 
+    def _input_exhange_type(self, message_str: str):
+        list_name: list = Safetypes.get_list()
+        self._connect_telebot.view_keyboard('Выберите тип сейфа:', list_name=list_name)
+        self._set_next_function(self._input_exhange_type_check)
+
+    def _input_exhange_type_check(self, message_str: str):
+        if Safetypes.check(message_str):
+            self._input_exhange(message_str)
+        else:
+            self._connect_telebot.send_text('Выбран неправильный тип сейфа.')
+            raise ExceptionOperationBank(f'Выбран не правильный тип сейфа - {message_str}')
+
     def _input_exhange(self, message_str: str):
-        self._model_safes_user = ModelSafeuser(self._connect_telebot.id_user)
-        safes_dict = self._model_safes_user.get_dict()
-        text_safes = ''
-        for safe in safes_dict.values():
-            text_safes += safe + ' '
-        self._connect_telebot.send_text(f'Введите биржу из списка: {text_safes}')
+        safes_dict = ModelSafeuser.get_dict(self._connect_telebot.id_user, type_name=message_str)
+        if not safes_dict:
+            safes_dict = {}
+        safes_dict['ДОБАВИТЬ'] = 'ДОБАВИТЬ'
+        self._connect_telebot.view_keyboard('Выберите сейф:', dict_name=safes_dict)
         self._set_next_function(self._input_exhange_check)
 
     def _input_exhange_check(self, message_str: str):
