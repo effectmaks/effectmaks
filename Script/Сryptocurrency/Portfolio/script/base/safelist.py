902eb83e17742de52bb8fe6c6af4992ea8f81b4c
Maksim Balonin <effectmaks@gmail.com>
2022-08-04 21:47:40 +0300

    добавил класс ModelSafelist, добавил метод command_create проверяет есть ли в базе такой тип сейфа с именем, если нет, то создает.

--- Script/Сryptocurrency/Portfolio/script/base/safelist.py
+++ Script/Сryptocurrency/Portfolio/script/base/safelist.py
@@ -15,8 +15,52 @@ class Safelist(Model):
     """
     id = IntegerField()
     name = TextField()
-    safetype = TextField()
+    type = TextField()
 
     class Meta:
         table_name = 'safelist'
         database = ConnectSqlite.get_connect()
+
+
+class ModelSafelist:
+    __name_model = 'safelist'
+
+    @classmethod
+    def __check(cls, name_safe: str, type_safe: str) -> int:
+        """
+        Проверка есть сейф
+        """
+        try:
+            list_safe = Safelist.select().where(Safelist.name == name_safe, Safelist.type == type_safe)
+            for sel in list_safe:
+                logging.info('Сейф уже есть в базе')
+                return sel.id
+            logging.info('Нет такого сейфа')
+        except Exception as err:
+            raise ExceptionSelect(cls.__name_model, str(err))
+
+    @classmethod
+    def __create(cls, name_safe: str, type_safe: str) -> int:
+        """
+        Прикрепление сейфа к юзеру
+        :param id_safe: ID сейфа
+        """
+        try:
+            id_safe = Safelist.create(name=name_safe, type=type_safe)
+            logging.info(f'Создан сейф name:{name_safe}, type:{type_safe} в базе')
+            return id_safe
+        except Exception as err:
+            raise ExceptionInsert(cls.__name_model, str(err))
+
+    @classmethod
+    def command_create(self, name_safe: str, type_safe: str) -> int:
+        """
+        Проверка есть ли такой сейф в списке.
+        Если сейфа нет - создаем.
+        """
+        logging.info(f'Проверка наличия name_safe:{name_safe}, type_safe:{type_safe} в базе')
+        id_safe = self.__check(name_safe, type_safe)
+        if id_safe:
+            return id_safe
+        else:
+            return self.__create(name_safe, type_safe)
