b456192d2fc758dd4abde856532f5cdaf4e4f4ff
Maksim Balonin <effectmaks@gmail.com>
2022-08-04 21:49:33 +0300

    добавил шаги _input_exhange, _input_exhange_check дл режима ввод средст на счет

--- Script/Сryptocurrency/Portfolio/script/business_model/operationBank.py
+++ Script/Сryptocurrency/Portfolio/script/business_model/operationBank.py
@@ -4,12 +4,6 @@ from base.safeuser import ModelSafeuser
 from telegram_bot.api.modesWork import ModesWork
 
 
-class StepsModeInput:
-    STOP = 'STOP'
-    EXCHANGE = 'EXCHANGE'
-    EXCHANGE_CHECK = 'EXCHANGE_CHECK'
-
-
 class OperationBank:
     """
     Операции ввода, вывода и конвертации средств
@@ -17,15 +11,45 @@ class OperationBank:
     def __init__(self, connect_telebot: ConnectTelebot, command_now: str):
         logging.info('Создание объекта OperationBank')
         self._command_now = command_now
-        if  self._command_now == ModesWork.COMMAND_INPUT:
-            self._step_now = StepsModeInput.EXCHANGE
         self._connect_telebot = connect_telebot
+        self._next_function = None
+        self._add_next_function = False
 
     def work(self, message_str: str):
-        if self._command_now == ModesWork.COMMAND_INPUT and self._step_now == StepsModeInput.EXCHANGE:
-            self._model_safes_user = ModelSafeuser(self._connect_telebot.id_user)
-            safes_dict = self._model_safes_user.get_dict()
-            text_safes = ''
-            for safe in safes_dict.values():
-                text_safes += safe + ' '
-            self._connect_telebot.send_text(f'Введите биржу из списка: {text_safes}')
+        if self._work_next_function(message_str):  # функция выполнилась
+            return
+        if self._command_now == ModesWork.COMMAND_INPUT:
+            self._step_exhange(message_str)
+
+    def _set_next_function(self, fnc):
+        """
+        Устанавливает следующая функция для вызова, когда придет новое сообщение
+        :param fnc: Следующая функция для вызова
+        """
+        self._next_function = fnc
+        self._add_next_function = True
+
+    def _work_next_function(self, message_str: str) -> bool:
+        """
+        Если было назначена следующая функция, то нужно выполнить
+        :param message_str: Текст сообщения пользователя
+        :return: Выполнилась функция
+        """
+        if self._next_function:
+            self._next_function(message_str)
+            if not self._add_next_function:
+                self._next_function = None
+                self._add_next_function = False
+            return True  # Выполнилась функция
+
+    def _input_exhange(self, message_str: str):
+        self._model_safes_user = ModelSafeuser(self._connect_telebot.id_user)
+        safes_dict = self._model_safes_user.get_dict()
+        text_safes = ''
+        for safe in safes_dict.values():
+            text_safes += safe + ' '
+        self._connect_telebot.send_text(f'Введите биржу из списка: {text_safes}')
+        self._set_next_function(self._input_exhange_check)
+
+    def _input_exhange_check(self, message_str: str):
+        self._connect_telebot.send_text(f'Введите 2')
--- Script/Сryptocurrency/Portfolio/script/main.py
+++ Script/Сryptocurrency/Portfolio/script/main.py
@@ -5,6 +5,8 @@ from base.cash import ModelCash
 from base.safeuser import ModelSafeuser
 from base.eventbank import ModelEventBank
 from base.cashsell import ModelCashSell
+from base.safelist import ModelSafelist, Safetypes
+
 
 
 import os
@@ -28,7 +30,8 @@ if __name__ == '__main__':
         #ModelSafeuser.test(1, 2)
         #id_cash = ModelCash.add(1, '01.08.22 10.00.00', 'ETH', 1.0, 10300.0)
         #id_cash_sell = ModelCashSell.add(1, '01.08.22 10.00.00', id_cash, 1.0, 10300.0)
-        ModelEventBank.add(1, 'INT', '01.08.22 10.00.00', 20, 0, fee=0.32, comment='sdf8520')
+        #ModelEventBank.add(1, 'INT', '01.08.22 10.00.00', 20, 0, fee=0.32, comment='sdf8520')
+        ModelSafelist.command_create('ETH', Safetypes.EXCHANGE)
 
     except Exception as e:
         print(str(e))
