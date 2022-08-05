eac3c54b4602e398d501f0e55516ef9da7fa703c
Maksim Balonin <effectmaks@gmail.com>
2022-08-05 22:49:56 +0300

    Класс OperationBank Добавил шаги ввода монеты, и создания новой.

--- Script/Сryptocurrency/Portfolio/script/business_model/operationBank.py
+++ Script/Сryptocurrency/Portfolio/script/business_model/operationBank.py
@@ -2,6 +2,8 @@ import logging
 from telegram_bot.api.telegramApi import ConnectTelebot
 from base.safeuser import ModelSafeuser
 from base.safelist import Safetypes, ModelSafelist
+from base.coin import ModelCoin
+from base.cash import ModelCash
 from telegram_bot.api.modesWork import ModesWork
 
 
@@ -22,10 +24,15 @@ class OperationBank:
         self._next_function = None
         self._add_next_function = False
         self._choice_safe_type: str
-        self._MODE_ADD_SAFE = 'ДОБАВИТЬ'
+        self._MODE_ADD = 'ДОБАВИТЬ'
         self._dict_safes_user = {}
         self._choice_safe_name: str
         self._choice_id_safe_user: int
+        self._TYPE_COIN = 'COIN'
+        self._TYPE_USD = 'USD'
+        self._choice_coin: str
+        self._coin_list = []
+
 
     def work(self, message_str: str):
         if self._work_next_function(message_str):  # функция выполнилась
@@ -85,7 +92,7 @@ class OperationBank:
         safes_dict = ModelSafeuser.get_dict(self._connect_telebot.id_user, type_name=self._choice_safe_type)
         if not safes_dict:
             safes_dict = {}
-        safes_dict[self._MODE_ADD_SAFE] = self._MODE_ADD_SAFE
+        safes_dict[self._MODE_ADD] = self._MODE_ADD
         self._dict_safes_user = safes_dict
         self._connect_telebot.view_keyboard('Выберите сейф:', dict_name=safes_dict)
         self._set_next_function(self._input_safe_list_check)
@@ -96,7 +103,7 @@ class OperationBank:
         Или переход на шаг создания нового сейфа с типом self._choice_safe_type.
         :param message_str: Ответ юзера
         """
-        if message_str == self._MODE_ADD_SAFE:
+        if message_str == self._MODE_ADD:
             self._create_safe_question()
         else:
             id_safe = self._dict_safes_user.get(message_str)
@@ -104,7 +111,7 @@ class OperationBank:
                 self._choice_safe_name = message_str
                 self._choice_id_safe_user = id_safe
                 logging.info(f'Выбран сейф ID_safe_user:{self._choice_id_safe_user} name:"{self._choice_safe_name}"')
-                self._input_coin()
+                self._input_coin_question()
             else:
                 self._connect_telebot.send_text(f'Такого сейфа "{message_str}" нет в списке.')
                 raise ExceptionOperationBank(f'Пользователь выбрал сейф - "{message_str}", он не из списка.')
@@ -112,7 +119,6 @@ class OperationBank:
     def _create_safe_question(self):
         """
         Вопрос - какое имя сейфа создавать?
-        :return:
         """
         logging.info(f'Режим создания сейфа с типом "{self._choice_safe_type}"')
         self._connect_telebot.send_text(f'Введите название сейфа с типом - "{self._choice_safe_type}":')
@@ -121,15 +127,68 @@ class OperationBank:
     def _create_safe_answer(self, message_str: str):
         """
         Создать новый сейф у юзера
-        :param message_str:
-        :return:
+        :param message_str: Ответ пользователя
         """
         logging.info(f'Режим проверки названия сейфа "{message_str}"')
         message_str = message_str.upper()
         id_safe_list = ModelSafelist.command_create(message_str, self._choice_safe_type)
         self._choice_id_safe_user = ModelSafeuser.command_create(self._connect_telebot.id_user, id_safe_list)
         self._connect_telebot.send_text(f'Добавлен новый сейф "{message_str}" с типом {self._choice_safe_type}.')
-        self._input_coin()
+        self._input_coin_question()
 
-    def _input_coin(self):
-        logging.info(f'Режим вопрос пользователю, какая монета?')
+    def _input_coin_question(self):
+        """
+        Выбрать монету
+        """
+        logging.info(f'Режим вопрос пользователю, выберите монету.')
+        coin_list = ModelCash.get_cash_coin(self._choice_id_safe_user)
+        if not coin_list:
+            coin_list = []
+        coin_list.append(self._MODE_ADD)
+        self._coin_list = coin_list
+        self._connect_telebot.view_keyboard('Выберите монету/валюту:', list_name=self._coin_list)
+        self._set_next_function(self._input_coin_answer)
+
+    def _input_coin_answer(self, message_str: str):
+        """
+        Проверить,монета из списка?
+        Иначе добавить.
+        :param message_str: Ответ пользователя
+        """
+        logging.info(f'Режим проверка монеты из списка.')
+        if message_str == self._MODE_ADD:
+            self._create_coin_question()
+        else:
+            if message_str in self._coin_list:
+                self._choice_coin = message_str
+                logging.info(f'Выбрана монета "{self._choice_coin}"')
+                self._input_amount_question()
+            else:
+                self._connect_telebot.send_text(f'Пункта "{message_str}" нет в списке.')
+                raise ExceptionOperationBank(f'Пользователь выбрал пункт - "{message_str}", он не из списка.')
+
+    def _create_coin_question(self):
+        """
+        Вопрос - какое имя монеты создавать?
+        """
+        logging.info(f'Режим создания монеты.')
+        coin_list = ModelCoin.get_list()
+        self._connect_telebot.view_keyboard('Напишите или выберите монету/валюту:', list_name=coin_list)
+        self._set_next_function(self._create_coin_answer)
+
+    def _create_coin_answer(self, message_str: str):
+        """
+        Создать новую монету
+        :param message_str: Ответ пользователя
+        """
+        logging.info(f'Режим проверки названия монеты "{message_str}"')
+        message_str = message_str.upper()
+        ModelCoin.command_create(message_str)
+        self._choice_coin = message_str
+        logging.info(f'Выбрана монета "{self._choice_coin}"')
+        self._input_amount_question()
+
+    def _input_amount_question(self):
+        logging.info(f'Режим вопроса объем пополнения')
+        self._connect_telebot.send_text(f'Введите объем пополнения:')
+
