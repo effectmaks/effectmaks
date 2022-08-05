bc9d418f75737a56f336b872e4f777c735692934
Maksim Balonin <effectmaks@gmail.com>
2022-08-05 21:20:47 +0300

    Добавил шаги режима input,  в класс OperationBank, методы _input_safe_list_check (Проверяет ответ пользователя, по типу сейфа), _input_safe_list(Режим показать сейфы с типом self._choice_safe_type), _input_safe_list_check (Проверка какой сейф выбрали с типом self._choice_safe_type), _create_safe_question(Вопрос - какое имя сейфа создавать?), _create_safe_answer (Создать новый сейф у юзера)

--- Script/Сryptocurrency/Portfolio/script/business_model/operationBank.py
+++ Script/Сryptocurrency/Portfolio/script/business_model/operationBank.py
@@ -1,7 +1,7 @@
 import logging
 from telegram_bot.api.telegramApi import ConnectTelebot
 from base.safeuser import ModelSafeuser
-from base.safelist import Safetypes
+from base.safelist import Safetypes, ModelSafelist
 from telegram_bot.api.modesWork import ModesWork
 
 
@@ -21,12 +21,17 @@ class OperationBank:
         self._connect_telebot = connect_telebot
         self._next_function = None
         self._add_next_function = False
+        self._choice_safe_type: str
+        self._MODE_ADD_SAFE = 'ДОБАВИТЬ'
+        self._dict_safes_user = {}
+        self._choice_safe_name: str
+        self._choice_id_safe_user: int
 
     def work(self, message_str: str):
         if self._work_next_function(message_str):  # функция выполнилась
             return
         if self._command_now == ModesWork.COMMAND_INPUT:
-            self._input_exchange_type(message_str)
+            self._input_safe_type()
 
     def _set_next_function(self, fnc):
         """
@@ -40,7 +45,7 @@ class OperationBank:
         """
         Если было назначена следующая функция, то нужно выполнить
         :param message_str: Текст сообщения пользователя
-        :return: Выполнилась функция
+        :return:True - выполнилась функция
         """
         if self._next_function:
             self._next_function(message_str)
@@ -49,25 +54,82 @@ class OperationBank:
                 self._add_next_function = False
             return True  # Выполнилась функция
 
-    def _input_exchange_type(self, message_str: str):
+    def _input_safe_type(self):
+        """
+        Формирование вопроса, на какой тип сейфа хотите пополнить.
+        """
+        logging.info(f'Режим задать вопрос, какой тип сейфа?')
         list_name: list = Safetypes.get_list()
         self._connect_telebot.view_keyboard('Выберите тип сейфа:', list_name=list_name)
-        self._set_next_function(self._input_exchange_type_check)
+        self._set_next_function(self._input_safe_type_check)
 
-    def _input_exchange_type_check(self, message_str: str):
+    def _input_safe_type_check(self, message_str: str):
+        """
+        Проверяет ответ пользователя, по типу сейфа.
+        Если правильно выбран, запоминает выбранный тип сейфа.
+        :param message_str: Ответ юзера
+        """
+        logging.info(f'Режим проверить "{message_str}" - это тип?')
         if Safetypes.check(message_str):
-            self._input_exchange(message_str)
+            self._choice_safe_type = message_str
+            self._input_safe_list()
         else:
             self._connect_telebot.send_text('Выбран неправильный тип сейфа.')
             raise ExceptionOperationBank(f'Выбран не правильный тип сейфа - {message_str}')
 
-    def _input_exchange(self, message_str: str):
-        safes_dict = ModelSafeuser.get_dict(self._connect_telebot.id_user, type_name=message_str)
+    def _input_safe_list(self):
+        """
+        Режим показать сейфы с типом self._choice_safe_type
+        """
+        logging.info(f'Режим показать сейфы с типом "{self._choice_safe_type}"')
+        safes_dict = ModelSafeuser.get_dict(self._connect_telebot.id_user, type_name=self._choice_safe_type)
         if not safes_dict:
             safes_dict = {}
-        safes_dict['ДОБАВИТЬ'] = 'ДОБАВИТЬ'
+        safes_dict[self._MODE_ADD_SAFE] = self._MODE_ADD_SAFE
+        self._dict_safes_user = safes_dict
         self._connect_telebot.view_keyboard('Выберите сейф:', dict_name=safes_dict)
-        self._set_next_function(self._input_exchange_check)
+        self._set_next_function(self._input_safe_list_check)
 
-    def _input_exchange_check(self, message_str: str):
-        self._connect_telebot.send_text(f'Введите 2')
+    def _input_safe_list_check(self, message_str: str):
+        """
+        Проверка какой сейф выбрали с типом self._choice_safe_type.
+        Или переход на шаг создания нового сейфа с типом self._choice_safe_type.
+        :param message_str: Ответ юзера
+        """
+        if message_str == self._MODE_ADD_SAFE:
+            self._create_safe_question()
+        else:
+            id_safe = self._dict_safes_user.get(message_str)
+            if id_safe:
+                self._choice_safe_name = message_str
+                self._choice_id_safe_user = id_safe
+                logging.info(f'Выбран сейф ID_safe_user:{self._choice_id_safe_user} name:"{self._choice_safe_name}"')
+                self._input_coin()
+            else:
+                self._connect_telebot.send_text(f'Такого сейфа "{message_str}" нет в списке.')
+                raise ExceptionOperationBank(f'Пользователь выбрал сейф - "{message_str}", он не из списка.')
+
+    def _create_safe_question(self):
+        """
+        Вопрос - какое имя сейфа создавать?
+        :return:
+        """
+        logging.info(f'Режим создания сейфа с типом "{self._choice_safe_type}"')
+        self._connect_telebot.send_text(f'Введите название сейфа с типом - "{self._choice_safe_type}":')
+        self._set_next_function(self._create_safe_answer)
+
+    def _create_safe_answer(self, message_str: str):
+        """
+        Создать новый сейф у юзера
+        :param message_str:
+        :return:
+        """
+        logging.info(f'Режим проверки названия сейфа "{message_str}"')
+        message_str = message_str.upper()
+        id_safe_list = ModelSafelist.command_create(message_str, self._choice_safe_type)
+        self._choice_id_safe_user = ModelSafeuser.command_create(self._connect_telebot.id_user, id_safe_list)
+        self._connect_telebot.send_text(f'Добавлен новый сейф "{message_str}" с типом {self._choice_safe_type}.')
+        self._input_coin()
+
+    def _input_coin(self):
+        logging.info(f'Режим вопрос пользователю, какая монета?')
