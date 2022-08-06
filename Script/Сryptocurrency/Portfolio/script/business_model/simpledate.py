c79b84ed69500f8c7d81e9aa78af5e177becb9fe
Maksim Balonin <effectmaks@gmail.com>
2022-08-06 09:33:18 +0300

    Класс SimpleDate добавил разные варианты ввода даты

--- Script/Сryptocurrency/Portfolio/script/business_model/simpledate.py
+++ Script/Сryptocurrency/Portfolio/script/business_model/simpledate.py
@@ -14,7 +14,28 @@ class SimpleDate:
     """
     @classmethod
     def convert(cls, date_time_str: str) -> datetime:
+        date_time = cls._variant_1(date_time_str)
+        if date_time:
+            return date_time
+        date_time = cls._variant_2(date_time_str)
+        if date_time:
+            return date_time
+        if not date_time:
+            raise ExceptionSimpleDate(f'cls:{cls.__name__} Ошибка конвертации даты и времени')
+
+    @classmethod
+    def _variant_1(cls, date_time_str: str) -> datetime:
         try:
-            return datetime.strptime(date_time_str, '%d.%m.%y %H.%M.%S')
-        except Exception as err:
-            raise ExceptionSimpleDate(f'Ошибка конвертации даты и времени {str(err)}')
+            template: str = '%d.%m.%y %H.%M.%S'
+            return datetime.strptime(date_time_str, template)
+        except Exception:
+            logging.info(f'cls:{cls.__name__} def:{cls._variant_1.__name__} Ошибка конвертации по шаблону "{template}"')
+
+    @classmethod
+    def _variant_2(cls, date_time_str: str) -> datetime:
+        try:
+            template: str = '%d.%m.%Y %H.%M.%S'
+            return datetime.strptime(date_time_str, template)
+        except Exception:
+            logging.info(f'cls:{cls.__name__} def:{cls._variant_2.__name__} Ошибка конвертации по шаблону "{template}"')
+
