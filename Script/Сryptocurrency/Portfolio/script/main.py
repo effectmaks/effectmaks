5b623569e82f500adf6179eeb1b5ce5b748ce3cb
Maksim Balonin <effectmaks@gmail.com>
2022-08-07 20:40:13 +0300

    кдасс Users, убрал баг по словарю dict_users, убрал message

--- Script/Сryptocurrency/Portfolio/script/main.py
+++ Script/Сryptocurrency/Portfolio/script/main.py
@@ -29,7 +29,7 @@ def start_bot():
 
     @bot_telegram.message_handler(content_types=['text'])
     def new_message(message):
-        Users.message_info(bot_telegram, message)
+        Users.message_info(bot_telegram, message.from_user.id, message.text)
 
     @bot_telegram.callback_query_handler(func=lambda call: True)
     def click_button(call):
--- Script/Сryptocurrency/Portfolio/script/telegram_bot/api/users.py
+++ Script/Сryptocurrency/Portfolio/script/telegram_bot/api/users.py
@@ -11,7 +11,7 @@ class Users:
     Класс пользователи контролирует многопользовательский режим.
     Каждый класс, это отдельный пользователь.
     """
-    dict_users: Dict[str, 'Users'] = {}
+    dict_users: Dict[int, 'Users'] = {}
 
     def __init__(self, botTelegram: TeleBot, id_user: int):
         self._id_user = id_user
@@ -19,28 +19,29 @@ class Users:
         self.control_bot = ControlBot(connect_telebot)
 
     @classmethod
-    def message_info(cls, bot_telegram: TeleBot, message):
+    def message_info(cls, bot_telegram: TeleBot, id_user: int, message_str: str):
         """
         Обрабатывает сообщения юзера.
         Создает нового пользователя или обращается к существующему.
+        :param message_str:
+        :param id_user:
         :param bot_telegram:
-        :param message:
         :return:
         """
         logging.info('>>>||')
-        user = cls.dict_users.get(message.from_user.id, None)
+        user = cls.dict_users.get(id_user, None)
         if user:
-            logging.info(f'Новое сообщение id_user:{message.from_user.id}')
+            logging.info(f'Новое сообщение id_user:{id_user}')
         else:
-            logging.info(f'Новый user_id:{message.from_user.id}')
-            TaskRule.check_delete(message.from_user.id)  # Проверяет базу на наличие запущенных заданий
-            user = Users(bot_telegram, message.from_user.id)
-            cls.dict_users[message.from_user.id] = user
+            logging.info(f'Новый user_id:{id_user}')
+            TaskRule.check_delete(id_user)  # Проверяет базу на наличие запущенных заданий
+            user = Users(bot_telegram, id_user)
+            cls.dict_users[id_user] = user
         try:
-            user.control_bot.new_message(message.text)
+            user.control_bot.new_message(message_str)
         except Exception as ex:
             logging.error(f'Серьезная ошибка в обработке данных {str(ex)}')
-            cls.dict_users.pop(message.from_user.id, None)
+            cls.dict_users.pop(id_user, None)
         logging.info('||>>>')
 
     @classmethod
@@ -52,7 +53,7 @@ class Users:
         :param message:
         :return:
         """
-        message = Message(call.from_user.id, call.data)
-        bot_telegram.edit_message_reply_markup(message.from_user.id, call.message.id)  # удалить клавиатуру
-        bot_telegram.send_message(call.from_user.id, message.text)  # отправить текст выбранной кнопки
-        Users.message_info(bot_telegram, message)
+        bot_telegram.delete_message(call.from_user.id, call.message.id)  # удалить текст клавиатуры
+        #bot_telegram.edit_message_reply_markup(call.from_user.id, call.message.id)  # удалить клавиатуру
+        # bot_telegram.send_message(call.from_user.id, message.text)  # отправить текст выбранной кнопки
+        Users.message_info(bot_telegram, call.from_user.id, call.data)
