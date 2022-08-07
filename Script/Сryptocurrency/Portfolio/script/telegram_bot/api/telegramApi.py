14848d98e66589ca63cd4a016a6461e86e1c74d7
Maksim Balonin <effectmaks@gmail.com>
2022-08-07 16:39:05 +0300

    класс ConnectTelebot теперь хранит сообщение от пользователя, метод view_keyboard_yes_no отображает кнопки вряд

--- Script/Сryptocurrency/Portfolio/script/telegram_bot/api/telegramApi.py
+++ Script/Сryptocurrency/Portfolio/script/telegram_bot/api/telegramApi.py
@@ -27,6 +27,7 @@ class ConnectTelebot:
     def __init__(self, telebot: TeleBot, id_user: int):
         self._telebot = telebot
         self._id_user = id_user
+        self.message: str = ''
 
     def send_text(self, text_send: str):
         logging.info(f'Отправить текст юзеру ID {self._id_user}: "{text_send}"')
@@ -52,6 +53,19 @@ class ConnectTelebot:
             for value in dict_name.keys():
                 key = types.InlineKeyboardButton(text=value, callback_data=value)
                 keyboard.add(key)  # добавляем кнопку
+        self._telebot.send_message(self._id_user, text=text_keyboard, reply_markup=keyboard)
+
+    def view_keyboard_yes_no(self, text_keyboard: str, const_yes: str, const_no: str):
+        """
+        Создание списка кнопок для юзера
+        :param dict_text: Словарь с названиями кнопок
+        :param text_keyboard: Вопрос для пользователя
+        """
+
+        key_yes = types.InlineKeyboardButton(text=const_yes, callback_data=const_yes)
+        key_no = types.InlineKeyboardButton(text=const_no, callback_data=const_no)
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.row(key_yes, key_no)
         self._telebot.send_message(self._id_user, text=text_keyboard, reply_markup=keyboard)
 
 
