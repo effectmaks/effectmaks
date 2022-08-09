5c69beb5908b6e391fcfff9bb274f14159ee723d
Maksim Balonin <effectmaks@gmail.com>
2022-08-09 22:34:49 +0300

    Переименовал поля в таблицах cash и cashsell, убрал _fiat

--- Script/Сryptocurrency/Portfolio/script/debug.py
+++ Script/Сryptocurrency/Portfolio/script/debug.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 from datetime import datetime
+
+from base.models.cash import Cash, ModelCash
 from business_model.taskrule import TaskRule
 from telegram_bot.api.commandsWork import CommandsWork
 
@@ -14,17 +16,6 @@ logging.basicConfig(
 )
 
 if __name__ == '__main__':
-    TaskRule.check_delete(id_user=481687938)
-    task = TaskRule(command_type=CommandsWork.COMMAND_INPUT)
-    task.date_time = datetime.now()
-    task.id_user = 481687938
-    task.safe_type = 'EXCHANGE'
-    task.safe_name = 'OKEX'
-    task.id_safe_user = 1
-    task.coin = 'ETH'
-    task.amount = 1
-    task.fee = 0.001
-    task.comment = '1234'
-    task.run()
+    cash = ModelCash.list_amount(4)
 
 
