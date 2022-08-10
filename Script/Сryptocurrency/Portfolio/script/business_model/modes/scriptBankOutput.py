204e3e3db4c748771e6039d4f7da6d2d537cd73b
Maksim Balonin <effectmaks@gmail.com>
2022-08-10 22:51:41 +0300

    класс ScriptBankOutput, выполняет режим bank_output

--- Script/Сryptocurrency/Portfolio/script/business_model/modes/scriptBankOutput.py
+++ Script/Сryptocurrency/Portfolio/script/business_model/modes/scriptBankOutput.py
@@ -1,6 +1,6 @@
 import logging
 
-from business_model.choice.choicecoin import ChoiceCoin
+from business_model.choice.choicecash import ChoiceCash
 from business_model.choice.choicefloat import ChoiceFloat
 from business_model.choice.choicetext import ChoiceText
 from business_model.nextfunction import NextFunction
@@ -25,12 +25,13 @@ class ScriptBankOutput:
         logging.info('Создание объекта OperationBank')
         self._connect_telebot = connect_telebot
         self._next_function = NextFunction(ScriptBankOutput.__name__)
-        self._next_function.set(self._work_simple_date)
+        self._next_function.set(self._work_choice_date)
         self._check_date_time: ChoiceDate = None
         self._choice_safe: ChoiceSafe = None
-        self._choice_coin: ChoiceCoin = None
-        self._choice_amount: ChoiceFloat = None
-        self._choice_fee: ChoiceFloat = None
+        self._choice_cash: ChoiceCash = None
+        self._choice_amount_first: ChoiceFloat = None
+        self._choice_amount_second: ChoiceFloat = None
+        self._fee: float
         self._choice_comment: ChoiceText = None
 
     def work(self):
@@ -40,7 +41,7 @@ class ScriptBankOutput:
         if self._next_function.work():  # функция выполнилась
             return
 
-    def _work_simple_date(self):
+    def _work_choice_date(self):
         """
         Команда сформировать дату и время
         """
@@ -48,7 +49,7 @@ class ScriptBankOutput:
             self._check_date_time = ChoiceDate(self._connect_telebot)
         working: bool = self._check_date_time.work()
         if working:
-            self._next_function.set(self._work_simple_date)
+            self._next_function.set(self._work_choice_date)
         else:
             logging.info('Выбран date_time')
             self._work_choice_safe()
@@ -64,46 +65,59 @@ class ScriptBankOutput:
             self._next_function.set(self._work_choice_safe)  # еще не выбрано, повторить
         else:
             logging.info('Выбран id_safe_user')
-            self._work_choice_coin()  # далее выполнить
+            self._work_choice_cash()  # далее выполнить
 
-    def _work_choice_coin(self):
+    def _work_choice_cash(self):
         """
-        Команда сформировать coin
+        Команда сформировать id_safe_user
         """
-        if not self._choice_coin:
-            self._choice_coin = ChoiceCoin(self._connect_telebot, self._choice_safe.result.id_safe)
-        working: bool = self._choice_coin.work()
+        if not self._choice_cash:
+            self._choice_cash = ChoiceCash(self._connect_telebot, self._choice_safe.result.id_safe)
+        working: bool = self._choice_cash.work()
         if working:
-            self._next_function.set(self._work_choice_coin)
+            self._next_function.set(self._work_choice_cash)  # еще не выбрано, повторить
         else:
-            logging.info('Выбран coin')
-            self._work_choice_float()  # далее выполнить
+            logging.info('Выбран id_cash')
+            self._work_choice_amount_first()  # далее выполнить
 
-    def _work_choice_float(self):
+    def _work_choice_amount_first(self):
         """
         Команда сформировать amount
         """
-        if not self._choice_amount:
-            self._choice_amount = ChoiceFloat(self._connect_telebot, question_main='Введите объем пополнения:')
-        working: bool = self._choice_amount.work()
+        if not self._choice_amount_first:
+            self._choice_amount_first = ChoiceFloat(self._connect_telebot,
+                                                    question_main='Введите сколько было выведено:',
+                                                    max_number=self._choice_cash.result.max_number)
+        working: bool = self._choice_amount_first.work()
         if working:
-            self._next_function.set(self._work_choice_float)  # еще не выбрано, повторить
+            self._next_function.set(self._work_choice_amount_first)  # еще не выбрано, повторить
         else:
-            logging.info('Выбран amount')
-            self._work_choice_fee()  # далее выполнить
+            logging.info('Выбран _choice_amount_first')
+            self._work_choice_amount_second()  # далее выполнить
 
-    def _work_choice_fee(self):
+    def _work_choice_amount_second(self):
         """
         Команда сформировать fee
         """
-        if not self._choice_fee:
-            self._choice_fee = ChoiceFloat(self._connect_telebot, question_main='Введите объем комиссии:')
-        working: bool = self._choice_fee.work()
+        if not self._choice_amount_second:
+            self._choice_amount_second = ChoiceFloat(self._connect_telebot,
+                                                     question_main='Введите какой объем получен:',
+                                                     max_number=self._choice_cash.result.max_number)
+        working: bool = self._choice_amount_second.work()
         if working:
-            self._next_function.set(self._work_choice_fee)  # еще не выбрано, повторить
+            self._next_function.set(self._work_choice_amount_second)  # еще не выбрано, повторить
         else:
-            logging.info('Выбран fee')
-            self._work_choice_comment()  # далее выполнить
+            logging.info('Выбран _choice_amount_second')
+            self._calc_fee()  # далее выполнить
+
+    def _calc_fee(self):
+        """
+        Вычисление комиссии
+        """
+        logging.info('Вычисление комиссии')
+        self._fee = self._choice_amount_first.result - self._choice_amount_second.result
+        self._connect_telebot.send_text(f'Комиссия составила: {self._fee}')
+        self._work_choice_comment()  # далее выполнить
 
     def _work_choice_comment(self):
         """
@@ -122,14 +136,12 @@ class ScriptBankOutput:
         """
         Создание задания на создание счета юзера
         """
-        task_rule = TaskRule(self._connect_telebot.id_user, CommandsWork.COMMAND_INPUT)
+        task_rule = TaskRule(self._connect_telebot.id_user, CommandsWork.COMMAND_OUTPUT)
         task_rule.date_time = self._check_date_time.result
-        task_rule.safe_type = self._choice_safe.result.safe_type
+        task_rule.id_cash = self._choice_cash.result.id_cash
         task_rule.id_safe_user = self._choice_safe.result.id_safe
-        task_rule.safe_name = self._choice_safe.result.safe_name
-        task_rule.coin = self._choice_coin.result
-        task_rule.amount = self._choice_amount.result
-        task_rule.fee = self._choice_fee.result
+        task_rule.amount = self._choice_amount_second.result
+        task_rule.fee = self._fee
         task_rule.comment = self._choice_comment.result
         task_rule.run()
         self._connect_telebot.send_text('Команда выполнена.')
